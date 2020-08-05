3ce5fce87dc9d32c55954bf28fadf33627526a23
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-08-05 19:39:55 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,20 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+  #This method should iterate over the hash and return the key (not the value!) that points to the smallest value of the set. If the method is called and passed an argument of an empty hash, it should return nil.
+
+
+
 
 def key_for_min_value(name_hash)
+  min_key = nil
+ min_value = nil
 
+
+ name_hash.each do |key, value|
+ if min_value == nil || value < min_value
+        min_value = value
+        min_key = key
+     end
+  end
+  min_key
 end
