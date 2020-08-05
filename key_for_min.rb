5851315640b5ddab6f10c4bae55f18da9d08a94d
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-08-05 19:12:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+#This method should iterate over the hash and return the key (not the value!) that points to the smallest value of the set. If the method is called and passed an argument of an empty hash, it should return nil.
+
+
+
 
 def key_for_min_value(name_hash)
 
